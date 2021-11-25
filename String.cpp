a5cd3fd07baccf29828b8666c558d7d291a4fcb9
Kulkarnisumedha <94223551+Kulkarnisumedha@users.noreply.github.com>
2021-11-25 18:45:40 +0530

    Add files via upload

---
+++ String.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main() {
+string firstname="sumedha";
+string lastname="kulkarni";
+cout<<"fullname:"<<firstname + lastname;"\n";
+cout<<"length of firstname:"<<firstname.size()<<"\n";
+cout<<"fullname:"<<firstname.append(lastname);
+return 0;
+}
